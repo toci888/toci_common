96c8f7f6996aa1f2aefbfbe95f11515b053c68bb
kacper8989 <kacper.wybierala1@gmail.com>
2022-02-28 20:59:09 +0100

    add Alphabet to string Utils

---
+++ Toci.Common/Toci.Common/StringUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Toci.Common
+{
+    public static class StringUtils
+    {
+        private static Random randomizer = new Random();
+        private static string alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
+        public static string GetRandomString(int maxLength)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < randomizer.Next(5, maxLength); i++)
+            {
+                result += alphabet[randomizer.Next(0, alphabet.Length-1)];
+            }
+
+            return result;
+        }
+    }
+}
