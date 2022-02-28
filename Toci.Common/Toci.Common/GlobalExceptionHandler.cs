3bb7d77d8c7f88949f18684d4db8ee1719b3d374
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2022-02-28 19:34:04 +0100

    global exception handling common logic.

---
+++ Toci.Common/Toci.Common/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toci.Common
+{
+    public class GlobalExceptionHandler
+    {
+        protected IErrorLogger ErrorLogger;
+
+        public GlobalExceptionHandler(IErrorLogger errorLogger)
+        {
+            ErrorLogger = errorLogger;
+        }
+
+        public virtual void ActivateGlobalExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionLog);
+        }
+
+        protected void UnhandledExceptionLog(object sender, UnhandledExceptionEventArgs exception)
+        {
+            List<Exception> excStack = new List<Exception>();
+
+            Exception masterExc = (Exception)exception.ExceptionObject;
+
+            excStack = BreakDownException(masterExc, excStack);
+
+            ErrorLogger.Log(excStack);
+        }
+
+        protected virtual List<Exception> BreakDownException(Exception masterExc, List<Exception> currentList)
+        {
+            currentList.Add(masterExc);
+
+            if (masterExc.InnerException != null)
+            {
+                BreakDownException(masterExc.InnerException, currentList);
+            }
+
+            return currentList;
+        }
+    }
+}
