83ecc50abaa85b310f33d67c8b2f1e83b5ab42af
Bartomiej Zapart <bzapart@hotmail.com>
2022-07-30 14:43:46 +0200

    returned response class.

---
+++ Toci.Common/Toci.Common.Bll/ComplexResponses/ReturnedResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Common.Bll.Errors
+{
+    public abstract class ReturnedResponse<TResult>
+    {
+        public TResult MethodResult { get; set; }
+
+        public string ErrorMessage { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
