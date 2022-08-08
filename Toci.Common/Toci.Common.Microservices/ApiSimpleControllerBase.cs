dc12281857b48c0c8046ed115577f09a2d61fad5
Bartomiej Zapart <bzapart@hotmail.com>
2022-08-08 15:08:36 +0200

    simple api pcontrolloer, without the model.

---
+++ Toci.Common/Toci.Common.Microservices/ApiSimpleControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Common.Microservices
+{
+    public abstract class ApiSimpleControllerBase<TLogic> : ControllerBase
+    {
+        protected TLogic Logic;
+        protected ApiSimpleControllerBase(TLogic logic)
+        {
+            Logic = logic;
+        }
+    }
+}
